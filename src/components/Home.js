1a2216ff3b99c2376baea681ef4e1677fbb2fa4f
Shivam yadav <shivamy@tekion.com>
2023-04-15 22:20:10 +0530

    added path config

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+
+import Image from 'next/image'
+
+const Home = ({ }) => {
+    return <div>
+     <Image 
+            src= '/images/Hero_Image.png'
+    width={400}
+    height={400}
+    />
+    </div>
+};
+
+
+export default Home;
