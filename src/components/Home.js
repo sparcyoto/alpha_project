debe89c3ea5a8a14e9c1f7256fb1f2f53c705d90
Shivam yadav <shivamy@tekion.com>
2023-04-15 22:50:03 +0530

    eslint configuration

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
+import Image from 'next/image';
+import React from 'react';
 
-import Image from 'next/image'
-
-const Home = ({ }) => {
-    return <div>
-     <Image 
-            src= '/images/Hero_Image.png'
-    width={400}
-    height={400}
-    />
+function Home(props) {
+  return (
+    <div>
+      <Image
+        src="/images/Hero_Image.png"
+        width={400}
+        height={400}
+      />
     </div>
-};
-
+  );
+}
 
 export default Home;
