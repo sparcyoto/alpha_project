965b5eb1504c48bd7a1e7c25d6db75f6aac25007
Shivam yadav <shivamy@tekion.com>
2023-04-17 01:09:48 +0530

    responsive design for the intro section

--- src/components/BrandPage/BrandPage.js
+++ src/components/BrandPage/BrandPage.js
@@ -20,13 +20,14 @@ function BrandPage(props) {
     );
 
     return (
-      <div className="flex mt-[8rem]">
+      <div className="flex-col  lg:flex lg:flex-row mt-[8rem]  ">
         <div className={styles.introContainer}>
-          <div className="flex">
+          <div className="flex items-center gap-x-2 text-[1rem] lg:gap-3">
             <Image
               src="/icons/red_tick.svg"
               width={30}
               height={30}
+              className="max-[500px]:w-[1.5rem]"
             />
             <div>₹ 0 Effective Fee</div>
           </div>
@@ -36,7 +37,7 @@ function BrandPage(props) {
             {renderRed('dream Internship/Job') }
           </div>
           <div> Get trained, work on live projects and get hired in 12 Weeks</div>
-          <div className="flex gap-x-4 mt-10 mb-4">
+          <div className="flex gap-x-4 mt-10 mb-4 max-[500px]:flex-col gap-y-3">
             <Button variation={BUTTON_VARIATION.PRIMARY} className="py-4 px-8">Register for Free</Button>
             <Button variation={BUTTON_VARIATION.SECONDARY} className="py-4 px-8">Explore Tracks</Button>
           </div>
