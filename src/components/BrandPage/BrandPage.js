2063673cc33a737d2c2b521d8d87f579c8e58cd2
Shivam yadav <shivamy@tekion.com>
2023-04-16 18:16:16 +0530

    scss suport

--- src/components/BrandPage/BrandPage.js
+++ src/components/BrandPage/BrandPage.js
@@ -1,11 +1,20 @@
 import Image from 'next/image';
 import React from 'react';
 
+import styles from './brandPage.module.scss';
+
 function BrandPage(props) {
+  const abc = 20;
+
   return (
-    <div>
+    <div className={styles.container}>
       <Image
         src="/images/Hero_Image.png"
+        width={400}
+        height={400}
+      />
+      <Image
+        src="/icons/tranity_logo.svg"
         width={400}
         height={400}
       />
